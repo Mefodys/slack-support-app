d9f140242b0f549f0732702c4a6f6b197187827a
Anton Mefodichev <anton.mefodichev@jetbrains.com>
2024-01-17 10:43:49 +0100

    fixed previous faulty code.

--- src/main/kotlin/Message3.kt
+++ src/main/kotlin/Message3.kt
@@ -1,9 +1,7 @@
-fun main() {
-    var iceCreamInventory = mutableMapOf('t' to 1, 'u' to 2)
-    iceCreamInventory.set('t', 5)
-    println(iceCreamInventory['t']) // prints: 5
-    println(iceCreamInventory) // prints: {t = 5, u = 2}
-    private var x = iceCreamInventory.getOrDefault('t', 0)
-    println(x) // prints: 1
+data class Message3(
+    val issueID: String? = null,
+    val issueType: String? = null,
+    val subsystem: String? = null,
+    val stateInYT: String? = null
+)
 
-}
