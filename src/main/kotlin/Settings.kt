3777bbc72a4ee6858a8240dbef0f285a71657ddc
Anton Mefodichev <anton.mefodichev@jetbrains.com>
2024-07-05 11:00:55 +0200

    new dates - 1.04.24-1.07.24

--- src/main/kotlin/Settings.kt
+++ src/main/kotlin/Settings.kt
@@ -7,7 +7,7 @@ object Settings {
 
     val channelToFetch = channelToId[CHANNEL_TO_FETCH]!!
 
-    val fromDate = dateToUnixTimestamp(2023, 9, 1).toString()
-    val tillDate = dateToUnixTimestamp(2023, 12, 1).toString()
+    val fromDate = dateToUnixTimestamp(2024, 4, 1).toString()
+    val tillDate = dateToUnixTimestamp(2024, 7, 1).toString()
 
 }
